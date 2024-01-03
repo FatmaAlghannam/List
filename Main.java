db08d1555cbcb64eadfe9cc7feb24792a56e934c
Fatimah A F A M Alghannam <83319@kfh.com>
2024-01-03 12:48:02 +0300

    array list

---
+++ Main.java
@@ -0,0 +1,35 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+public class Main {
+  public static void main(String[] args) {
+    
+     List<String> Hobbies = new ArrayList<>();
+    Hobbies.add("Song1");
+    Hobbies.add("Book11");
+    Hobbies.add("Book2");
+    Hobbies.add("Song2");
+    Hobbies.add("Movie1");
+
+   for (String Hobb: Hobbies) {
+
+     System.out.println(Hobb);
+    
+   }
+                             
+  
+
+
+
+
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
+
